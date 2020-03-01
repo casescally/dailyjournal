92ea5820911f4dd652864c7164e4fed063febcf2
Case Scally <casescally@gmail.com>
2020-02-29 19:47:03 -0600

    refactored flexbox, fixed button text, added placeholder text.

--- scripts/JournalComponent.js
+++ scripts/JournalComponent.js
@@ -2,39 +2,39 @@ const JournalComponent = () => {
   return `
   <form action="" class="" id="entryForm">
   <fieldset class="form">
-    <h1>Daily Journal</h1>
-    <div class="entries">
+    <h1 class="heading">Daily Journal</h1>
+    <div class="entry__date">
       <label for="journal-date">Date of entry:</label>
-      <br>
+
       <input type="date" name="journal-date" id="journal-date">
     </div>
-    <br>
-    <div class="entries">
+
+    <div class="entry__conceptsCovered">
       <label for="concepts-covered">Concepts Covered:</label>
-      <br>
-      <input type="text" name="concepts-covered" id="concepts-covered">
+
+      <input type="text" name="concepts-covered" id="concepts-covered" placeHolder="Subject">
     </div>
     <br>
-    <div class="entries">
+    <div class="entry__body">
       <label for="journal-entry">Journal Entry</label>
-      <br>
-      <textarea rows="5" cols="50" name="journal-entry" id="journal-entry">Type here</textarea>
+
+      <textarea rows="5" cols="50" name="journal-entry" id="journal-entry" placeHolder="What did you learn?"></textarea>
     </div>
     <br>
-    <div class="entries">
+    <div class="entry__mood">
       <label for="mood">How do you feel about it?</label>
-      <br>
+
       <select name="mood" id="mood">
         <option value="Good">Good</option>
         <option value="Neutral">Neutral</option>
         <option value="Bad">Bad</option>
       </select>
     </div>
-    <br>
+
   </fieldset>
   </form>
 
-  <div class="btn">
+  <div class="buttons">
   <button id="submit" class="button">Submit</button>
   <button id="showEntries" class="button">Show Entries</button>
   </div>
